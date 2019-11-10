566dfbf5b37b726ec9437472a429157f162075e0
amr <amrreda547@gmail.com>
2019-11-10 15:20:34 +0200

    add vulnerable secret

--- index.js
+++ index.js
@@ -15,6 +15,26 @@ const app = express()
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
+// authorization middleware
+const API_KEY = 'zaCELgL.0imfnc8mVLWwsAawjYr4Rx-Af50DDqtlx'
+app.use(function (req, res, next) {
+    var reqToken = req.get('Authorization');
+    
+    if (reqToken) {
+        if (reqToken == 'Bearer '+ API_KEY) {
+            next();
+        } else {
+            res.status(401).json({
+                "message":"Not Authorized"
+            })
+        }
+    } else {
+        res.status(401).json({
+            "message":"Authorization header not found"
+        })
+    }
+})
+
 // todo item routes
 router.use('/item', require('./routes/index'));
 app.use('/', router)
